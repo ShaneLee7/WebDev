3855dfd2af627b8af8463a6225c74fc97c4804ec
ShaneLee7 <shanesaulee@gmail.com>
2024-11-04 04:12:06 +0000

    models migrations seeders

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body'];
+
+    // Relationship with User
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relationship with Comments
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route 1: Returns a string
-Route::get('/route1', function () {
-    return "Hello World";
-});
-
-// Route 2: Redirects to Route 1
-Route::get('/route2', function () {
-    return redirect('/route1');
-});
-
-// Route 3: Accepts a string parameter
-Route::get('/output/{string}', function ($string) {
-    return response($string);
+Route::get('/', function () {
+    return view('welcome');
 });
