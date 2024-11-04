0145a30c530e60669dc0ea8ceb7c18f8582d5d72
ShaneLee7 <shanesaulee@gmail.com>
2024-11-04 03:42:58 +0000

    Added 2 routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route 1: Returns a string
+Route::get('/route1', function () {
+    return "Hello World";
+});
+
+// Route 2: Redirects to Route 1
+Route::get('/route2', function () {
+    return redirect('/route1');
+});
+
+// Route 3: Accepts a string parameter
+Route::get('/output/{string}', function ($string) {
+    return response($string);
 });
