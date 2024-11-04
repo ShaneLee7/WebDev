d3ee880c17892d49a2925847eea2b01a889d031f
ShaneLee7 <shanesaulee@gmail.com>
2024-11-04 05:18:26 +0000

    added factory and seeders

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(), // Create a new user for the comment
+            'post_id' => Post::factory(), // Create a new post for the comment
+            'body' => $this->faker->sentence, // Random sentence for the comment
+        ];
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(), // Create a new user for the post
+            'title' => $this->faker->sentence,  // Random tile
+            'body' => $this->faker->paragraph,  // Random sentence for the comment body
+        ];
+    }
+}
